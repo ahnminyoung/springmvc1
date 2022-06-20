5e76762b955ee37996e75273192980fa3a5ea68d
anrh0 <anrh0213@nate.com>
2022-06-20 17:49:06 +0900

    + 로그에 대해

---
+++ src/main/java/hello/springmvc1/basic/LogTestController.java
@@ -0,0 +1,26 @@
+package hello.springmvc1.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController // return 하는거 문자를 리턴하면 그럼 string이 그대로 반환이됨
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
